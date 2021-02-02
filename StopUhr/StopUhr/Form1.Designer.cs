1048dbd39da2044acaa498c82fce38ed6bdcfa86
RussiaPlayer <wiskystar2@gmail.com>
2021-02-02 19:21:34 +0100

    Revert "Rainbow Background"
    This reverts commit 2554fb59845a32441213107029300c01b1ac3cbf.

--- StopUhr/StopUhr/Form1.Designer.cs
+++ StopUhr/StopUhr/Form1.Designer.cs
@@ -66,7 +66,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(323, 170);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.stopBT);
---
+++ StopUhr/StopUhr/RainbowBackground.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace StopUhr
+{
+    class RainbowBackground
+    {
+        private int Red, Green, Blue;
+        public Color RainbowBG(long RGspeed, bool an)
+        {
+            System.Timers.Timer RGtimer = new System.Timers.Timer();
+            RGtimer.Interval = RGspeed;
+            RGtimer.AutoReset = true;
+            RGtimer.Elapsed += RGtimerEvent;
+
+            if (an)
+            {
+                RGtimer.Start();
+            }
+            else if (!an)
+            {
+                RGtimer.Stop();
+            }
+
+            return Color.FromArgb(Red, Green, Blue);
+        }
+
+        private void RGtimerEvent(object sender, ElapsedEventArgs e)
+        {
+            if (Red == 255 && Green < 255 && Blue == 0)
+            {
+                ++Green;
+            }
+            else if (Red > 0 && Green == 255 && Blue == 0)
+            {
+                --Red;
+            }
+            else if (Red == 0 && Green == 255 && Blue < 255)
+            {
+                ++Blue;
+            }
+            else if (Red == 0 && Green > 0 && Blue == 255)
+            {
+                --Green;
+            }
+            else if (Red < 255 && Green == 0 && Blue == 255)
+            {
+                ++Red;
+            }
+            else if (Red == 255 && Green == 0 && Blue > 0)
+            {
+                --Blue;
+            }
+        }
+    }
+}
