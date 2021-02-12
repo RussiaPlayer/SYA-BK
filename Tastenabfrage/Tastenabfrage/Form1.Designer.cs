42d0eb033e20516dfc94e5a46ff278ead69867a5
RussiaPlayer <wiskystar2@gmail.com>
2021-02-12 09:09:28 +0100

    New Project

---
+++ Tastenabfrage/Tastenabfrage/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Tastenabfrage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tastenabfrage IOW 28";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(70, 283);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(117, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(214, 283);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(117, 100);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(357, 283);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(117, 100);
+            this.panel3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Tastenabfrage/Tastenabfrage/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace Tastenabfrage
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern int IowKitOpenDevice();
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitCloseDevice(int iowHandle);
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitWrite(int iowHandle, int numPipe, ref byte[] buffer, int length);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitGetDeviceHandle(int numDevice);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitGetNumDevs();
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitReadNonBlocking(int iowHandle, int numPipe, ref byte buffer, int length);
+
+
+        private int handle;
+        private System.Timers.Timer aTimer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            aTimer.Interval = 50;
+            aTimer.Elapsed += OnTimerElapsed;
+            aTimer.Start();
+
+            for (int i = 1; i < IowKitGetNumDevs(); i++)
+            {
+                handle = IowKitGetDeviceHandle(i);
+            }
+        }
+
+        byte[] data = new byte[5];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            data[0] = 0x00;
+            data[1] = 0xE0;
+            data[2] = 0x00;
+            data[3] = 0x00;
+            data[4] = 0x00;
+            IowKitWrite(handle, 0, ref data, 5);
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            label2.Text = IowKitReadNonBlocking(handle, 0, ref data[2], 5).ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IowKitCloseDevice(handle);
+            aTimer.Stop();
+        }
+    }
+}
