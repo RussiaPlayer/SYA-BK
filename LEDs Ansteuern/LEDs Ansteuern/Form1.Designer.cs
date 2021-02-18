764ff8c59ab984fe403831325a3b1164535e1e76
RussiaPlayer <wiskystar2@gmail.com>
2021-02-18 14:23:10 +0100

    Neues Projekt angelegt

---
+++ LEDs Ansteuern/LEDs Ansteuern/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LEDs_Ansteuern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(89, 91);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Grüne LED";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(89, 167);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(78, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Gelbe LED";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(89, 252);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(73, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Rote LED";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Location = new System.Drawing.Point(360, 68);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 72);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(360, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(75, 72);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(360, 224);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(75, 72);
+            this.panel3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(555, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(555, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LEDs Ansteuern/LEDs Ansteuern/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace LEDs_Ansteuern
+{
+    public partial class Form1 : Form
+    {
+        //Dll-start
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern int IowKitOpenDevice();
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitCloseDevice(int iowHandle);
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitWrite(int iowHandle, int numPipe, ref byte buffer, int length);
+        //Dll-stop
+
+
+        private byte[] data = new byte[5];
+        //LEDs
+        private const byte pin17 = 1 << 0;  //grün
+        private const byte pin18 = 1 << 1;  //gleb
+        private const byte pin19 = 1 << 2;  //rot
+
+        int handle;
+
+        public Form1()
+        {
+            InitializeComponent();
+            handle = IowKitOpenDevice();
+
+            //Panels-color
+            panel1.BackColor = Color.FromArgb(21, 66, 35);
+            panel2.BackColor = Color.FromArgb(82, 78, 24);
+            panel3.BackColor = Color.FromArgb(92, 28, 28);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Write on Iow
+            data[0] = 0;
+            data[1] = 0;
+            data[2] = pin17 | pin18 | pin19;
+            data[3] = 0;
+            data[4] = 0;
+            IowKitWrite(handle, 0, ref data[0], 5);
+
+            //Labels
+            label1.Text = "Data[1] = " + String.Format(" {0:X2} ", data[1]) + " ( " + data[1].ToString() + " ) ";
+            label2.Text = "Data[2] = " + String.Format(" {0:X2} ", data[2]) + " ( " + data[2].ToString() + " ) ";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IowKitCloseDevice(handle);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            LEDs(checkBox1.Checked, 1, panel1);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            LEDs(checkBox2.Checked, 2, panel2);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            LEDs(checkBox3.Checked, 3, panel3);
+        }
+
+        private void LEDs(bool active, byte pin, Panel panel)
+        {
+            if (active)
+            {
+                data[2] = (byte)(data[2] & ~(1 << (pin - 1)));
+            }
+            else
+            {
+                data[2] = (byte)(data[2] | 1 << (pin - 1));
+            }
+            IowKitWrite(handle, 0, ref data[0], 5);
+            ChangePanal(panel, pin, active);
+            ChangeLabels();
+        }
+
+        private void ChangePanal(Panel panel, byte pin, bool active)
+        {
+            if (active)
+            {
+                switch (pin)
+                {
+                    case 1:
+                        panel.BackColor = Color.FromArgb(25, 191, 77);
+                        break;
+                    case 2:
+                        panel.BackColor = Color.FromArgb(222, 210, 55);
+                        break;
+                    case 3:
+                        panel.BackColor = Color.FromArgb(214, 39, 39);
+                        break;
+                }
+            }
+            else
+            {
+                switch (pin)
+                {
+                    case 1:
+                        panel.BackColor = Color.FromArgb(21, 66, 35);
+                        break;
+                    case 2:
+                        panel.BackColor = Color.FromArgb(82, 78, 24);
+                        break;
+                    case 3:
+                        panel.BackColor = Color.FromArgb(92, 28, 28);
+                        break;
+                }
+            }
+        }
+         private void ChangeLabels()
+        {
+            label1.Text = "Data[1] = " + String.Format(" {0:X2} ", data[1]) + " ( " + data[1].ToString() + " ) ";
+            label2.Text = "Data[2] = " + String.Format(" {0:X2} ", data[2]) + " ( " + data[2].ToString() + " ) ";
+        }
+
+        //LaufLED
+    }
+}
