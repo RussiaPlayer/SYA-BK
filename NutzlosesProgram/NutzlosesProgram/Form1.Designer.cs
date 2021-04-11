cd64af1c4efadcefc82884dc07d319351ec620e9
RussiaPlayer <wiskystar2@gmail.com>
2021-04-11 11:53:40 +0200

    New Project

---
+++ NutzlosesProgram/NutzlosesProgram/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NutzlosesProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Counter = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.Updater = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Counter
+            // 
+            this.Counter.AutoSize = true;
+            this.Counter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Counter.Location = new System.Drawing.Point(192, 9);
+            this.Counter.Name = "Counter";
+            this.Counter.Size = new System.Drawing.Size(24, 25);
+            this.Counter.TabIndex = 0;
+            this.Counter.Text = "0";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(164, 84);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.TabStop = false;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // Updater
+            // 
+            this.Updater.Interval = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 119);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.Counter);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TastenCounter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label Counter;
+        private System.Windows.Forms.Timer Updater;
+    }
+}
+
---
+++ NutzlosesProgram/NutzlosesProgram/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NutzlosesProgram
+{
+    public partial class Form1 : Form
+    {
+        
+        int counter = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form_KeyDown);
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            counter++;
+            UpdateLabel();
+        }
+
+        private void keypressed(Object o, KeyPressEventArgs e)
+        {
+            counter++;
+            UpdateLabel();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ResetCounter();
+        }
+
+        private void ResetCounter()
+        {
+            counter = 0;
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            Counter.Text = Convert.ToString(counter);
+        }
+    }
+}
