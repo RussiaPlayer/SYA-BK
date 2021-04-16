1900dcc9ac0a24e46a8c6c86572cad53841b0cac
RussiaPlayer <wiskystar2@gmail.com>
2021-04-17 01:11:30 +0200

    spawning obstacle

--- CarCrashing/CarCrashing/Form1.cs
+++ CarCrashing/CarCrashing/Form1.cs
@@ -13,19 +13,21 @@ namespace CarCrashing
     public partial class Form1 : Form
     {
         byte playerPosition;
+        Random rnd = new Random();
 
         public Form1()
         {
             InitializeComponent();
             this.KeyPreview = true;
             this.KeyDown += new KeyEventHandler(MovePlayer);
+            timer1.Start();
 
             SpawnPlayer();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            SpawnObstacle();
         }
 
         void SpawnPlayer()
@@ -74,6 +76,30 @@ namespace CarCrashing
                     break;
                 case 4:
                     panel4.BackColor = Color.Red;
+                    break;
+            }
+        }
+
+        void SpawnObstacle()
+        {
+            panel13.BackColor = Color.Green;
+            panel14.BackColor = Color.Green;
+            panel15.BackColor = Color.Green;
+            panel16.BackColor = Color.Green;
+
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+                    panel13.BackColor = Color.FromArgb(224, 224, 224);
+                    break;
+                case 2:
+                    panel14.BackColor = Color.FromArgb(224, 224, 224);
+                    break;
+                case 3:
+                    panel15.BackColor = Color.FromArgb(224, 224, 224);
+                    break;
+                case 4:
+                    panel16.BackColor = Color.FromArgb(224, 224, 224);
                     break;
             }
         }
